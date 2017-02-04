104ffdbba0ccacaf2c881f0764fc088eb72861fa
cshenton <charlieshenton@me.com>
2017-02-04 15:11:44 +1100

    first pass at tokeniser

--- markov/tokeniser.py
+++ markov/tokeniser.py
@@ -1,9 +1,19 @@
-def tokeniser(string, depth):
+def tokenise(passage, depth):
     """
-    string: a passage of text to split into markov chain token - result pairs
+    passage: a passage of text to split into markov chain token - result pairs
     depth: how many words/punctuation, should be in each token
 
     Returns a dict of token - result pairs, where a result of None
     denotes the end of a passage of text.
     """
-    pass
+
+    tokens = passage.split()
+    token_dict = {}
+
+    for i in range(len(tokens)-depth+1):
+        if i < len(tokens)-depth:
+            token_dict[tuple(tokens[i:(i+depth)])] = tokens[i+depth]
+        else:
+            token_dict[tuple(tokens[i:(i+depth)])] = None
+
+    return token_dict
