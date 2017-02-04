7ea1cd67a17052859732b9e81638dd1a3d06b945
cshenton <charlieshenton@me.com>
2017-02-04 15:52:17 +1100

    added Distribution class for dist of results

--- markov/chain.py
+++ markov/chain.py
@@ -1,3 +1,36 @@
+from numpy.random import choice
+
+class Distribution:
+    """
+    Represents the distribution of results for a particular token
+    """
+    def __init__(self, value):
+        self.targets = {value: 1.0}
+        self.obs = 1
+
+    def update(self, new_value):
+        """
+        Updates the distribution of values given the newly observed value
+        """
+        for k,v in self.targets.items():
+            if k == new_value:
+                self.targets[k] = (self.obs*v + 1)/(self.obs + 1)
+            else:
+                self.targets[k] = (self.obs*v)/(self.obs + 1)
+
+        if new_value not in self.targets.keys():
+            self.targets[new_value] = 1 / (self.obs + 1)
+
+        self.obs += 1
+
+    def choose(self):
+        """
+        Randomly select a value according to the target probability distibution
+        """
+        outcome = choice(list(self.targets.keys()),
+                         p=list(self.targets.values()))
+        return outcome
+
 class Chain:
     def __init__(self, depth):
         self.token_hash = {}
@@ -10,7 +43,8 @@ class Chain:
 
         updates the token hash based on the token - result pair
         """
-        pass
+
+
 
     def simulate(self, start=None):
         """
@@ -19,3 +53,12 @@ class Chain:
         generates a string based on the
         """
         pass
+
+
+
+
+{'foo': 0.8, 'bar', 0.2}
+
+
+self.obs*prob
+self.obs+1
